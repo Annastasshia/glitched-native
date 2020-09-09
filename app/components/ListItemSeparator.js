58f753f2c481148dae72c9511bbea1f001f65bc2
Annastasshia Ames <aames26@gmail.com>
2020-09-08 20:53:01 -0400

    seperators and selections add

---
+++ app/components/ListItemSeparator.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { StyleSheet, View } from 'react-native'
+import colors from '../config/colors'
+
+export default class ListItemSeparator extends Component {
+    render() {
+        return (
+            <View style={styles.separator} />
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    separator: {
+        width: "100%",
+        height: 1,
+        backgroundColor: colors.light,
+    },
+})
--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native'
 
 import ListItem from '../components/ListItem'
 import Screen from "../components/Screen";
+import ListItemSeparator from '../components/ListItemSeparator';
 
 
 
@@ -28,16 +29,20 @@ const messages = [
 export default function MessagesScreen() {
     return (
         <Screen>
-        <FlatList
-        data={messages}
-        keyExtractor={message => message.id.toString()}
-        renderItem={({ item }) =>
-            <ListItem
-                title={item.title}
-                subTitle={item.description}
-                image={item.image} /> 
-            } />
-            </Screen>
+            <FlatList
+            data={messages}
+            keyExtractor={message => message.id.toString()}
+            renderItem={({ item }) => (
+                <ListItem
+                    title={item.title}
+                    subTitle={item.description}
+                    image={item.image}
+                    onPress={() => console.log("Message selected")} 
+                /> 
+             )} 
+               ItemSeparatorComponent={ListItemSeparator}
+            />
+        </Screen>
     )
 }
 
